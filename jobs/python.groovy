786dc23a853269e6c0b1dca33434fb1c0b200184
Your Name <you@example.com>
2017-09-07 21:55:05 +0100

    python.groovy

---
+++ jobs/python.groovy
@@ -0,0 +1,26 @@
+project_name = "jenkins-pipeline-python"
+repo = "git@github.com:videocursoscloud/test-jenkins-1.git"
+repo_name = "repo"
+
+
+pipelineJob(project_name) {
+    definition {
+       triggers{
+          scm('H/1 * * * *')
+       }
+
+        cpsScm{
+            scm {
+                git {
+                    remote {
+                    name(repo_name)
+                    url(repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+
+            }
+        }
+    }
+}
+
