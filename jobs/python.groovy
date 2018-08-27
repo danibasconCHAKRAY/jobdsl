94c660a8d7192944a968188ce6c03c8bad3bbd61
danibasconCHAKRAY <daniel.bascon@chakray.co.uk>
2018-08-27 10:22:23 +0000

    Nuevo groovy

---
+++ jobs/python.groovy
@@ -0,0 +1,23 @@
+project_name = "jenkins-pipeline-python"
+repo = "git@github.com:danibasconCHAKRAY/ansible-jnekins-cicd.git"
+repo_name = "repo"
+
+pipelineJob(project_name) {
+	definition {
+		triggers{
+			scm("H/1 * * * *")
+		}
+
+		cps5cm{
+			scm {
+				git {
+					remote {
+						name(repo_name)
+						url{repo}
+					}
+				}
+				scriptPath("Jenkinsfile")
+			}
+		}
+	}
+}
