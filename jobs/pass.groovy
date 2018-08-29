a53ebdebb3d8543eedaf8b67975ca8f572d17a9c
danibasconCHAKRAY <daniel.bascon@chakray.co.uk>
2018-08-29 08:33:03 +0000

    New groovy

---
+++ jobs/pass.groovy
@@ -0,0 +1,31 @@
+job("vmware-pass") {
+	description("password user")
+	keepDependencies(false)
+	scm {
+		git {
+			remote {
+				github("danibasconCHAKRAY/prueba", "ssh")
+			}
+			branch("*/master")
+		}
+	}
+	disabled(false)
+	concurrentBuild(false)
+	steps {
+		shell("""#!/bin/bash
+
+cd vagrant-esxi
+pwd
+ls
+set +x
+vagrant plugin install vagrant-vmware-esxi
+export esxi_password=\$VMWARE
+
+vagrant up prueba  --provider=vmware_esxi --provision""")
+	}
+	wrappers {
+		credentialsBinding {
+			string("c8ca2f47-777a-4ac1-85c8-c4b50c880f32", "VMWARE")
+		}
+	}
+}
